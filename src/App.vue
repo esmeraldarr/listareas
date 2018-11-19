80eff5369a0703cacb8bf3687f14e91ab1955bf7
y.ramirez <yramirez@mma.gob.cl>
2018-11-19 16:35:55 -0300

    primer commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app" class="container">
+    <div class="jumbotron">
+      <titulo :titulo="titulo"></titulo>
+      <nueva-tarea v-bind:tareas="tareas"></nueva-tarea>
+      <lista-tarea :listaT="listaT"></lista-tarea>
+    </div>
+  </div>
+</template>
+
+<script>
+import Titulo from './Titulo.vue'
+import NuevaTarea from './nuevaTarea.vue'
+import ListaTareas from './listaTareas.vue'
+export default {
+    components: {
+        Titulo,
+        NuevaTarea,
+        ListaTareas
+    },
+    data(){
+        return {
+            titulo: 'lista de tareas',
+            tareas: [
+            {
+              texto: 'Aprender Vue.js',
+              terminada: false
+            },
+            {
+              texto: 'Aprender Angular 2',
+              terminada: false
+            },
+            {
+              texto: 'Aprender Ionic 2',
+              terminada: false
+            }
+          ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/listaTareas.vue
@@ -0,0 +1,27 @@
+<template>
+	<ul class="list-group">
+	    <li v-for="(tarea, indice) of tareas" class="list-group-item" 
+	        v-bind:class="{terminada: tarea.terminada}">
+	      {{ tarea.texto }}
+	      <span class="pull-right">
+	      <button type="button" class="btn btn-success btn-xs glyphicon glyphicon-ok"
+	              v-on:click="tarea.terminada = !tarea.terminada"       
+	      ></button>
+	      <button type="button" class="btn btn-danger btn-xs glyphicon glyphicon-remove"
+	              v-on:click="borrar(indice)"       
+	      ></button>
+	    </span>
+	    </li>
+  	</ul>
+</template>
+<script>
+export default{
+	props:['tareas']
+}
+</script>
+<style>
+.terminada{
+    color: gray;
+    text-decoration: line-through;
+  }
+</style>
